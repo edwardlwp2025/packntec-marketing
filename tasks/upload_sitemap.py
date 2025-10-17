7c5e3bcd3381bd02f885f7d79efe7e1543e674f1
edwardlwp2025 <edwardlwp@gmail.com>
2025-10-16 19:05:51 -0700

    Update upload_sitemap.py

--- tasks/upload_sitemap.py
+++ tasks/upload_sitemap.py
@@ -1,24 +1,21 @@
-# tasks/upload_sitemap.py
-import os
-import paramiko  # installed via requirements.txt
+# tasks/upload_sitemap.py  (no utils import)
+import os, paramiko
 
 LOCAL  = "reports/sitemap.xml"
-REMOTE = os.environ.get("REMOTE_SITEMAP_PATH", "/home/z09esrwccxu0/public_html/sitemap.xml")  # override in workflow
+REMOTE = os.environ.get("REMOTE_SITEMAP_PATH", "/home/z09esrwccxu0/public_html/sitemap.xml")  # overridden in workflow
 
-def sftp_upload(local_path: str, remote_path: str):
+def sftp_upload(local_path, remote_path):
     host = os.environ["SFTP_HOST"]
     port = int(os.environ.get("SFTP_PORT", "22"))
     user = os.environ["SFTP_USER"]
     pwd  = os.environ["SFTP_PASS"]
-
     t = paramiko.Transport((host, port))
     t.connect(username=user, password=pwd)
     s = paramiko.SFTPClient.from_transport(t)
     s.put(local_path, remote_path)
-    s.close()
-    t.close()
+    s.close(); t.close()
 
-if all(os.getenv(k) for k in ("SFTP_HOST", "SFTP_USER", "SFTP_PASS")):
+if all(os.getenv(k) for k in ("SFTP_HOST","SFTP_USER","SFTP_PASS")):
     print(f"Uploading {LOCAL} -> {REMOTE}")
     sftp_upload(LOCAL, REMOTE)
     print("Sitemap uploaded.")
